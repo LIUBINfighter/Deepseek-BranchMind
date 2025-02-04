6b01c776d2a95122288e3b8cf89160f7da832202
LIUBINfighter <142153595+LIUBINfighter@users.noreply.github.com>
2025-02-05 06:15:07 +0800

    github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import path from 'path'
 
 export default defineConfig({
+  base: './', // 添加这一行
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
